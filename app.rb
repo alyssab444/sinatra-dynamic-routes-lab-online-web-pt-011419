72c70addbf78733039a534e617998d70bfb469e1
Alyssa Brown <alyssa.brown@live.com>
2019-04-17 00:08:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,7 +23,8 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-    "#{@word1+ @word2+ @word3+ @word4+ @word5}.".join 
+    separate = "#{@word1+ @word2+ @word3+ @word4+ @word5}."
+    separate.split 
     binding.pry 
   
   end 
